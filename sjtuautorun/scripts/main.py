1a46c05b86dba09d07ac3cd365b27b121cd1545a
Cao Shuchen <csc123456zxcvbnm@gmail.com>
2024-02-27 17:04:00 +0800

    add: route to settings.json

--- sjtuautorun/scripts/main.py
+++ sjtuautorun/scripts/main.py
@@ -2,17 +2,26 @@ import datetime
 import os
 from types import SimpleNamespace
 
+import sjtuautorun
 from sjtuautorun.controller.run_timer import Emulator, Timer
-from sjtuautorun.utils.io import yaml_to_dict
+from sjtuautorun.utils.io import recursive_dict_update, yaml_to_dict
 from sjtuautorun.utils.new_logger import Logger
 
 event_pressed = set()
 script_end = 0
 
 
-def initialize_logger_and_config():
-    current_folder = os.path.dirname(os.path.abspath(__file__))
-    config = yaml_to_dict(os.path.join(current_folder, os.pardir, os.pardir, "settings.yaml"))
+def initialize_logger_and_config(settings_path):
+    config = yaml_to_dict(os.path.join(os.path.dirname(sjtuautorun.__file__), "data", "default_settings.yaml"))
+    if settings_path is not None:
+        user_settings = yaml_to_dict(settings_path)
+        config = recursive_dict_update(config, user_settings)
+    else:
+        print("========Warning========")
+        print(f"No user_settings file specified, default settings "
+              f"{os.path.join(os.path.dirname(sjtuautorun.__file__), 'data', 'default_settings.yaml')}"
+              f" will be used.")
+        print("=========End===========")
 
     # set logger
     config["log_dir"] = os.path.join(config["LOG_PATH"], datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
@@ -24,7 +33,7 @@ def initialize_logger_and_config():
     return config, logger
 
 
-def start_script():
+def start_script(settings_path):
     """启动脚本, 返回一个 Timer 记录器.
     :如果模拟器没有运行, 会尝试启动模拟器,
     :如果游戏没有运行, 会自动启动交我办,
@@ -32,16 +41,16 @@ def start_script():
     Returns:
         Timer: 该模拟器的记录器
     """
-    config, logger = initialize_logger_and_config()
+    config, logger = initialize_logger_and_config(settings_path)
     timer = Timer(config, logger)
     return timer
 
 
-def start_script_emulator():
+def start_script_emulator(settings_path):
     """启动脚本, 返回一个 Emulator 记录器, 用于操作模拟器
     Returns:
         Emulator: 该模拟器的记录器
     """
-    config, logger = initialize_logger_and_config()
+    config, logger = initialize_logger_and_config(settings_path)
     emulator = Emulator(config, logger)
     return emulator
--- start.py
+++ start.py
@@ -1,2 +1,4 @@
+import os
+
 from sjtuautorun.scripts.main import start_script
-start_script()
+start_script(f"{os.path.dirname(os.path.abspath(__file__))}/settings.yaml")
