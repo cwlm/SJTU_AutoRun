c8348c13c44eeb1d4c36b35a215efb762f49bb93
cwlm <csc123456zxcvbnm@gmail.com>
2024-03-04 14:07:06 +0800

    fix: teleporting at start

--- sjtuautorun/mygo/run_plan.py
+++ sjtuautorun/mygo/run_plan.py
@@ -27,6 +27,11 @@ class RunPlan:
         assert len(plan_args["points"]) >= 2, "请输入两个以上途径点"
 
     def start_run(self):
+
+        time.sleep(5)
+        # 初始化位置
+        self.timer.change_location(self.plan_args["points"][0][0], self.plan_args["points"][0][1])
+
         pos = self.timer.wait_image(IMG.run_image[1])
         self.timer.Android.click(pos[0], pos[1])
 
@@ -34,25 +39,23 @@ class RunPlan:
             self.run()
 
     def run(self):
-        # 初始化位置
-        self.timer.change_location(self.plan_args["points"][0][0], self.plan_args["points"][0][1])
-
-        # 启动跑步
-        # 还没做
-        time.sleep(5)
-
+        time.sleep(3)
         for i in range(len(self.plan_args["points"]) - 1):
-            start_longitude = self.plan_args["points"][i - 1][0]
-            end_longitude = self.plan_args["points"][i][0]
-            start_latitude = self.plan_args["points"][i - 1][1]
-            end_latitude = self.plan_args["points"][i][1]
+
+            start_longitude = self.plan_args["points"][i][0]
+            end_longitude = self.plan_args["points"][i + 1][0]
+            start_latitude = self.plan_args["points"][i][1]
+            end_latitude = self.plan_args["points"][i + 1][1]
 
             total_distance = calculate_geo_distance(start_latitude, start_longitude, end_latitude, end_longitude)
+            interval = 0.5
             running_pace = random.uniform(3.5, 4)
-            step_distance = 5 / (3 * running_pace)
+            step_distance = 1000 * interval / (60 * running_pace)
             num_steps = round(total_distance / step_distance)
 
             for j in range(1, num_steps):
+                start_time = time.time()
+
                 # 计算经纬度的插值
                 current_longitude = np.interp(j, [1, num_steps - 1], [start_longitude, end_longitude])
                 current_latitude = np.interp(j, [1, num_steps - 1], [start_latitude, end_latitude])
@@ -61,5 +64,6 @@ class RunPlan:
                 self.timer.change_location(current_longitude, current_latitude)
                 self.timer.logger.debug(current_longitude, current_latitude)
 
-                # 等待0.5秒
-                time.sleep(0.1)
+                # 等待0.1秒
+                elapsed_time = time.time() - start_time
+                time.sleep(max(0.0, interval - elapsed_time))
