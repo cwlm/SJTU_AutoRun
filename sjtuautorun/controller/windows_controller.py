7ddc308045b8bdafc945f77995252973de442b06
cwlm <csc123456zxcvbnm@gmail.com>
2024-03-11 23:13:20 +0800

    fix: 模拟器路径有空格时报错

--- sjtuautorun/controller/windows_controller.py
+++ sjtuautorun/controller/windows_controller.py
@@ -2,7 +2,6 @@ import os
 import re
 import subprocess
 import time
-from subprocess import check_output
 
 import airtest.core.android.android
 from airtest.core.api import connect_device
@@ -60,11 +59,20 @@ class WindowsController:
         :rtype: str
         """
         console_dir = os.path.join(os.path.dirname(self.emulator_dir), "ldconsole.exe")
+
         if not global_command:
-            ret = os.popen(console_dir + " " + command + " --index " + str(self.emulator_index) + " " + command_arg)
+            cmd = [console_dir, command, "--index", str(self.emulator_index), command_arg]
         else:
-            ret = os.popen(console_dir + " " + command_arg)
-        return ret.read()
+            cmd = [console_dir, command_arg]
+
+        # 使用subprocess.Popen来执行命令
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        output, error = process.communicate()
+
+        # 获取命令执行的输出
+        result = output.decode("utf-8", errors="replace") if output else error.decode("utf-8", errors="replace")
+
+        return result
 
     # @try_for_times()
     def connect_android(self) -> airtest.core.android.android.Android:
