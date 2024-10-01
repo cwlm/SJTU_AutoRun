5a5138f3bb03f50d7555e692faf5380fc8c34790
cwlm_ma <cwlm_ma@sjtu.edu.cn>
2024-10-01 10:01:04 +0800

    fix: code quality

--- sjtuautorun/utils/operator.py
+++ sjtuautorun/utils/operator.py
@@ -4,7 +4,7 @@ from typing import Iterable
 def remove_0_value_from_dict(d: dict):
     rd = {}
     for key, value in d.items():
-        if value != 0 and value != None:
+        if value != 0 and value is not None:
             rd[key] = value
     return rd
 
@@ -21,7 +21,7 @@ def unzip_element(o: object):
     return res
 
 
-def unorder_equal(o1: Iterable, o2: Iterable, skip=[None]):
+def unordered_equal(o1: Iterable, o2: Iterable, skip=[None]):
     for obj in o1:
         if obj not in o2 and obj not in skip:
             return False
